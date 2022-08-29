254b4cff687b2b4f9843aec74e03cf17842b05d7
Ekaterina <missathlon08@gmail.com>
2022-08-29 22:20:36 +0300

    add Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Vvedite imya polzovatelya: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ura, eto zhe Masha!");
+}
+else
+{
+    Console.Write("Privet, ");
+    Console.WriteLine(username);
+}
